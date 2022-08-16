9d264d02d5aa9d41493e8efd2cb11641b21f8e66
khan <40911737+aphichatkhan@users.noreply.github.com>
2022-08-16 09:56:28 +0700

    up

---
+++ main.py
@@ -0,0 +1,389 @@
+# !/usr/bin/python
+# -- coding: utf-8 --
+import json
+from time import sleep
+from escpos.printer import Network
+import requests
+from PIL import Image, ImageFont, ImageOps, ImageDraw
+# import mysql.connector
+from multiprocessing import Process
+
+
+# ip_host = "http://172.104.184.60/ipsoftapi/"
+ip_host = "http://165.22.59.74/"
+
+# printer_ipAddress = "192.168.1.251"
+
+get_ip_printer = requests.get(
+    url=ip_host+'api/printerlists',
+    params=dict(origin='Chicago,IL',destination='Los+Angeles,CA',waypoints='Joplin,MO|Oklahoma+City,OK',sensor='false')
+    )
+
+ip_printer_data = get_ip_printer.json()
+
+def printer_Order(ip_printer,type,kitchen,table,customer,item,order_id,order,created_at):
+    p = Network(ip_printer)
+    p.set(align='left')
+    if type == "บุฟเฟ่":
+        p.image(textImage(u"บุฟเฟ่ต์"))
+        p.image(textImage(u"ครัว : "+kitchen))
+    else:
+        p.image(textImage(u"ทานที่ร้าน"))
+        p.image(textImage(u"ครัว : อาหาร"))
+    p.image(textImage(table))
+    p.image(textImage(u"ลูกค้า : "+customer))
+    p.text('------------------------------------------------')
+    p.text('------------------------------------------------ \n')
+    for item in item:
+        textDetail = u"     " + str(item["amount"]) + "   " + item["foodName"]
+        if len(textDetail) > 45:
+            p.image(textImage(textDetail[:45]))
+            p.image(textImage(textDetail[45:]))
+        else:
+            p.image(textImage(textDetail))
+            
+        if item["description"] != None:
+            textDescription = u"       ***"+ item["description"]
+            
+            if len(textDescription) > 45:
+                p.image(textImage(textDescription[:45]))
+                p.image(textImage(textDescription[45:]))
+            else:
+                p.image(textImage(textDescription))
+            
+        if len(item["toping"]) != 0:
+            for item2 in item["toping"]:
+                if item2["amount"] != None:
+                    if item2["amount"] > 0:
+                        textTopping = u"         + " + str(item2["amount"]) + " " + item2["topingName"]
+                    else:
+                        textTopping = u"         + " + item2["topingName"]
+                else:
+                    textTopping = u"         + " + item2["topingName"]
+                if len(textTopping) > 45:
+                    p.image(textImage(textTopping[:45]))
+                    p.image(textImage(textTopping[45:]))
+                else:
+                    p.image(textImage(textTopping))
+    p.text('\n')
+    p.text('------------------------------------------------')
+    p.text('------------------------------------------------ \n')
+    p.image(textImage(u'ออเดอร์ที่ : #' + str(order)))
+    p.image(textImage(created_at))
+    p.cut()
+    
+    if type == "บุฟเฟ่":
+        url2 = ip_host+'api/updateOrderDetail'
+        data = {
+            'order_detail_id': order_id,
+            'printer_id': item["printer_id"],
+            'status_printer': 1
+        }
+    else:
+        url2 = ip_host+'api/updateOrderDetailnobuff'
+        data = {
+            'order_detail_id': order_id,
+            'status_printer': 1
+        }
+    res = requests.post(url2,json=data)
+    return print("Print Order To Kidchen")
+    
+
+def textImage(text):
+    font = ImageFont.truetype('ThaiSarabun/THSarabunNew Bold.ttf', 45)
+    box1 = font.getsize(text)
+    image = Image.new('RGB', (box1[0], 10 + box1[1]))
+    draw1 = ImageDraw.Draw(image)
+    draw1.text((0, 0), text, font=font)
+    textImage = ImageOps.invert(image)
+    return textImage
+
+def textImageBill(text):
+    font = ImageFont.truetype('ThaiSarabun/THSarabunNew Bold.ttf', 35)
+    box1 = font.getsize(text)
+    image = Image.new('RGB', (box1[0], 10 + box1[1]))
+    draw1 = ImageDraw.Draw(image)
+    draw1.text((0, 0), text, font=font)
+    textImage = ImageOps.invert(image)
+    return textImage
+
+def qrcode():
+    url = ip_host+'api/print_qr_code'
+    params = dict(
+        origin='Chicago,IL',
+        destination='Los+Angeles,CA',
+        waypoints='Joplin,MO|Oklahoma+City,OK',
+        sensor='false'
+    )
+    try:
+        res = requests.get(url=url, params=params)
+        data = res.json()
+        
+        p = Network(ip_printer_data[1]["IP_address"])
+        p.set(align='center')
+        p.image(Image.open(requests.get(data["logo_image"], stream=True).raw))
+        p.text('------------------------------------------------ \n')
+        if data["type"] == "บุฟเฟ่":
+            p.image(textImage(u"บุฟเฟ่ต์"))
+            p.image(textImage(u"โต๊ะที่ : "+data["table"]))
+        else:
+            p.image(textImage(u"ทานที่ร้าน"))
+            p.image(textImage(u"โต๊ะที่ : "+data["table"]))
+        p.text('\n')
+        p.image(Image.open(requests.get(data["qrcode_image"], stream=True).raw))
+        p.text('\n')
+        p.image(textImage("ขอบคุณที่มาอุดหนุน"))
+        p.image(textImage("powerd by ZoftConnect"))
+        p.cut()
+        
+        url2 = ip_host+'api/updateopentable'
+        data = {
+            'id':data["opentable_id"]
+        }
+        res = requests.post(url2,json=data)
+        print('Print Qrcode')
+    except:
+        pass
+    
+def orderTokidchen():
+    url = ip_host+'api/check-order-to-kitchen'
+    params = dict(
+        origin='Chicago,IL',
+        destination='Los+Angeles,CA',
+        waypoints='Joplin,MO|Oklahoma+City,OK',
+        sensor='false'
+    )
+    
+    try:
+        res = requests.get(url=url, params=params)
+        data = res.json()
+        
+        for detail in data["detail"]:
+            if len(detail["printer_1"]) > 0:
+                printer_Order(
+                    ip_printer_data[0]["IP_address"],
+                    data["type"],
+                    "ครัว 1",
+                    data["table"],
+                    data["customer"],
+                    detail["printer_1"],
+                    data["order_id"],
+                    data["order"],
+                    data["created_at"]
+                    )
+            if len(detail["printer_2"]) > 0:
+                printer_Order(
+                    ip_printer_data[1]["IP_address"],
+                    data["type"],
+                    "ครัว 2",
+                    data["table"],
+                    data["customer"],
+                    detail["printer_2"],
+                    data["order_id"],
+                    data["order"],
+                    data["created_at"]
+                )
+            if len(detail["printer_3"]) > 0:
+                printer_Order(
+                    ip_printer_data[2]["IP_address"],
+                    data["type"],
+                    "ครัว 3",
+                    data["table"],
+                    data["customer"],
+                    detail["printer_3"],
+                    data["order_id"],
+                    data["order"],
+                    data["created_at"]
+                )
+            if len(detail["printer_4"]) > 0:
+                printer_Order(
+                    ip_printer_data[3]["IP_address"],
+                    data["type"],
+                    "ครัว 4",
+                    data["table"],
+                    data["customer"],
+                    detail["printer_4"],
+                    data["order_id"],
+                    data["order"],
+                    data["created_at"]
+                )
+            if len(detail["printer_5"]) > 0:
+                printer_Order(
+                    ip_printer_data[4]["IP_address"],
+                    data["type"],
+                    "ครัว 5",
+                    data["table"],
+                    data["customer"],
+                    detail["printer_5"],
+                    data["order_id"],
+                    data["order"],
+                    data["created_at"]
+                )
+            if len(detail["printer_6"]) > 0:
+                printer_Order(
+                    ip_printer_data[5]["IP_address"],
+                    data["type"],
+                    "ครัว 6",
+                    data["table"],
+                    data["customer"],
+                    detail["printer_6"],
+                    data["order_id"],
+                    data["order"],
+                    data["created_at"]
+                )
+        
+        print('Print Order To Kitchen')
+    except:
+        pass
+    
+def orderTableTakehome():
+    url = ip_host+'api/ordertabletakehome'
+    params = dict(
+        origin='Chicago,IL',
+        destination='Los+Angeles,CA',
+        waypoints='Joplin,MO|Oklahoma+City,OK',
+        sensor='false'
+    )
+    try:
+        res = requests.get(url=url, params=params)
+        data = res.json()
+        
+        p = Network(ip_printer_data[1]["IP_address"])
+        p.set(align='center')
+        p.image('take-away.png')
+        p.set(align='left')
+        p.image(textImage(data["invoiceDetail"]["customer_name"]))
+        p.image(textImage(data["invoiceDetail"]["invoiceNumber"]))
+        p.text('------------------------------------------------')
+        p.text('------------------------------------------------ \n')
+        for item in data["invoiceDetail"]["listnoBuffet"]:
+            textDetail = u"     " + str(item["amount"]) + "   " + item["name"]
+            if len(textDetail) > 45:
+                p.image(textImage(textDetail[:45]))
+                p.image(textImage(textDetail[45:]))
+            else:
+                p.image(textImage(textDetail))
+                
+            if item["description"] != None:
+                textDescription = u"       ***"+ item["description"]
+                
+                if len(textDescription) > 45:
+                    p.image(textImage(textDescription[:45]))
+                    p.image(textImage(textDescription[45:]))
+                else:
+                    p.image(textImage(textDescription))
+                    
+            if len(item["topping"]) != 0:
+                for item2 in item["topping"]:
+                    if item2["amount"] != None:
+                        if item2["amount"] > 0:
+                            textTopping = u"         + " + str(item2["amount"]) + " " + item2["topingName"]
+                        else:
+                            textTopping = u"         + " + item2["topingName"]
+                    else:
+                        textTopping = u"         + " + item2["topingName"]
+                    if len(textTopping) > 45:
+                        p.image(textImage(textTopping[:45]))
+                        p.image(textImage(textTopping[45:]))
+                    else:
+                        p.image(textImage(textTopping))
+                    
+        p.text('\n')
+        p.text('------------------------------------------------')
+        p.text('------------------------------------------------ \n')
+        p.image(textImage(u'ออเดอร์ที่ : #' + str(data["invoiceDetail"]["order"])))
+        p.image(textImage(data["invoiceDetail"]["created_at"]))
+        p.cut()
+        
+        url2 = ip_host+'api/updateorder'
+        data = {
+            'id':data["invoiceDetail"]["order_id"],
+            'status':1
+        }
+        res = requests.post(url2,json=data)
+        print('Print Order Table Take Home')
+    except:
+        pass
+    
+def orderTakeHome():
+    url = ip_host+'api/ordertakehome'
+    params = dict(
+        origin='Chicago,IL',
+        destination='Los+Angeles,CA',
+        waypoints='Joplin,MO|Oklahoma+City,OK',
+        sensor='false'
+    )
+    
+    try:
+        res = requests.get(url=url, params=params)
+        data = res.json()
+        
+        # print(data["detail"])
+        
+        p = Network(ip_printer_data[1]["IP_address"])
+        p.set(align='center')
+        p.image('take-away.png')
+        p.set(align='left')
+        p.image(textImage(u"คุณ "+data["customer_name"]))
+        p.image(textImage(data["invoiceNumber"]))
+        p.text('------------------------------------------------')
+        p.text('------------------------------------------------ \n')
+        for detail in data["detail"]:
+            for item in detail["printer_2"]:
+                textDetail = u"     " + str(item["amount"]) + "   " + item["foodName"]
+                if len(textDetail) > 45:
+                    p.image(textImage(textDetail[:45]))
+                    p.image(textImage(textDetail[45:]))
+                else:
+                    p.image(textImage(textDetail))
+                    
+                if item["description"] != None:
+                    textDescription = u"       - "+ item["description"]
+                    
+                    if len(textDescription) > 45:
+                        p.image(textImage(textDescription[:45]))
+                        p.image(textImage(textDescription[45:]))
+                    else:
+                        p.image(textImage(textDescription))
+                        
+                if len(item["toping"]) != 0:
+                    for item2 in item["toping"]:
+                        if item2["amount"] != None:
+                            if item2["amount"] > 0:
+                                textTopping = u"         + " + str(item2["amount"]) + " " + item2["topingName"]
+                            else:
+                                textTopping = u"         + " + item2["topingName"]
+                        else:
+                            textTopping = u"         + " + item2["topingName"]
+                        if len(textTopping) > 45:
+                            p.image(textImage(textTopping[:45]))
+                            p.image(textImage(textTopping[45:]))
+                        else:
+                            p.image(textImage(textTopping))
+        p.text('\n')
+        p.text('------------------------------------------------')
+        p.text('------------------------------------------------ \n')
+        p.image(textImage(u'ออเดอร์ที่ : #' + str(data["order"])))
+        p.image(textImage(data["created_at"]))
+        p.cut()
+        
+        url2 = ip_host+'api/updatetakehome'
+        data = {
+            'id':data["invoice_id"],
+            'status':1
+        }
+        res = requests.post(url2,json=data)
+        print('Print Order Take Home')
+    except:
+        pass
+    
+
+if __name__ == "__main__":
+    while True:
+        qrcode()
+        orderTokidchen()
+        orderTableTakehome()
+        orderTakeHome()
+        sleep(1)
+
+# orderTokidchen()
