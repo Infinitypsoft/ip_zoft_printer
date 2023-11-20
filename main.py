70058dd4d98b4fda473d195af5532d629d7e39ad
ZoftConnect <zoftconnect@gmail.com>
2023-11-20 15:31:57 +0700

    update samkana

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageFont, ImageOps, ImageDraw
 from multiprocessing import Process
 
 
-ip_host = "http://170.187.227.77/ipsoftapi/"
+ip_host = "http://192.53.173.122/ipsoftapi/"
 # ip_host = "http://172.104.184.60/ipsoftapi/"
 # ip_host = "http://165.22.59.74/"
 # ip_host = "http://localhost:8000/"
