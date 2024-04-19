4e7f7d9a46a1a3aaa2a310fdf866963d95768d3f
ZoftConnect <zoftconnect@gmail.com>
2024-04-19 13:41:30 +0700

    update order

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageFont, ImageOps, ImageDraw
 from multiprocessing import Process
 
 
-ip_host = "http://170.187.228.220/ipsoftapi/"
+ip_host = "https://demo.zoftconnect.co/ipsoftapi/"
 # ip_host = "http://172.104.184.60/ipsoftapi/"
 # ip_host = "http://165.22.59.74/"
 #ip_host = "http://localhost:8000/"
